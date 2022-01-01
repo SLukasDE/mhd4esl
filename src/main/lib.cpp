d51dc2ae6de1de61265b64eb90fd6f6721c7b974
Sven Lukas <Sven.Lukas@googlemail.com>
2022-01-01 17:19:31 +0100

    update to new version

--- src/main/lib.cpp
+++ src/main/lib.cpp
@@ -1,3 +1,21 @@
+/*
+ * This file is part of mhd4esl.
+ * Copyright (C) 2019-2022 Sven Lukas
+ *
+ * Mhd4esl is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Mhd4esl is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser Public License
+ * along with mhd4esl.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <mhd4esl/Module.h>
 
 #include <esl/Module.h>
